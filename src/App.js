223e23d982e4ff96eb13c0a2e1b3feaf8b3c80b6
King Kong <king-kong@gmail.com>
2023-01-23 15:08:54 -0800

    books link shows list of books, /home brings back home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route path="/books">
+          <Link exact to="/">
+            Home
+          </Link>
+          <BookList />
+        </Route>
+
+        <Route path="/">
+          <Link to="/books">Books</Link>
+        </Route>
+      </Switch>
     </main>
   );
 }
